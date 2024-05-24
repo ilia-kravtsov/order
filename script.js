5b63318577207e44caa2fb19dc3b417c2abe2d2e
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2024-05-24 08:17:23 +0400

    swiper adding

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log(1);
+const usePhotoSlider = () => {
+  new Swiper(".swiper", {
+    direction: "horizontal",
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+};
+usePhotoSlider();
